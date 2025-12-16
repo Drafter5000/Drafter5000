ff52f733cc2629df1b0781bfbbf1639882ffebcd
harshad-ladva-fullstack <harshad.ladva.fullstack@gmail.com>
2025-12-16 12:01:39 +0530

    fix: solved the formatting issue

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -79,7 +79,7 @@ export function DashboardHeader() {
             </div>
           </div>
           <div className="flex items-center gap-2">
-            {/* <Button
+            {/* <Button∏∏∏
               variant="outline"
               size="sm"
               onClick={toggleAndReload}
